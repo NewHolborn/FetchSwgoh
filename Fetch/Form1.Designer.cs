6d319dd239857ba128791a8b28215a1406b4fe1a
NewHolborn <33021952+NewHolborn@users.noreply.github.com>
2017-10-26 15:27:43 +0300

    Add files via upload

---
+++ Fetch/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Fetch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butFetch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butFetch
+            // 
+            this.butFetch.Location = new System.Drawing.Point(12, 12);
+            this.butFetch.Name = "butFetch";
+            this.butFetch.Size = new System.Drawing.Size(75, 23);
+            this.butFetch.TabIndex = 0;
+            this.butFetch.Text = "Fetch";
+            this.butFetch.UseVisualStyleBackColor = true;
+            this.butFetch.Click += new System.EventHandler(this.butFetch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 192);
+            this.Controls.Add(this.butFetch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butFetch;
+    }
+}
+
---
+++ Fetch/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fetch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Net.WebClient web = null;
+        private void butFetch_Click(object sender, EventArgs e)
+        {
+            string html = String.Empty;
+            try
+            {
+                web = new System.Net.WebClient();
+                web.DownloadStringCompleted += new System.Net.DownloadStringCompletedEventHandler(web_DownloadStringCompleted);
+                Uri uri = new Uri("https://swgoh.gg/u/tsitas_66/collection/237/grand-admiral-thrawn/");
+                web.DownloadStringAsync(uri);
+            }
+            catch
+            {
+            }
+        }
+        void web_DownloadStringCompleted(object sender, System.Net.DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+            }
+            else
+            {
+                string html = e.Result;
+                int index = html.IndexOf("<h5>General</h5>", html.Length / 2);
+                if (index != -1)
+                {
+                    string retver = html.Substring(index);
+                    string health = "";
+                    for (int i = 17; i < 160; i++)
+                    {
+                        if (char.IsNumber(retver[i]))
+                        {
+                            health += retver[i];
+                        }
+                    }
+
+                    index = retver.IndexOf("Health");
+                    if (index != -1)
+                    {
+                        retver = retver.Substring(index);
+                        string protection = "";
+                        for (int i = 0; i < 150; i++)
+                        {
+                            if (char.IsNumber(retver[i]))
+                            {
+                                protection += retver[i];
+                            }
+                        }
+                        MessageBox.Show("Health:" + health + "\r\nProtection:" + protection);
+                    }
+
+
+                }
+            }
+        }
+    }
+}
